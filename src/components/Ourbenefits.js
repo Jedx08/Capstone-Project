5a3b8437d4d31b2822758171f7e44fbe7bac5605
Jedx08 <jedsaez08@gmail.com>
2022-08-28 12:16:09 +0800

    ourben_rentawrks final

--- src/components/Ourbenefits.js
+++ src/components/Ourbenefits.js
@@ -2,34 +2,35 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import '../css/ourbenefits.css';
 
 const Ourbenefits = () => {
   return (
-    <div>
+    <div className='ourb'>
       <Container className='text-center p-4'>
-        <h3 style={{color: "#333333"}}>OUR BENEFITS</h3>
-        <em style={{color: "#999999"}}>Luxury AutoStar Rental Services</em>
+        <h3>OUR BENEFITS</h3>
+        <em>Luxury RentCar Rental Services</em>
       <Row className='mt-5 justify-content-md-between'>
         <Col sm lg="3">
             <img src='https://autostar.templines.org/wp-content/uploads/2018/12/as001.jpg' />
-            <h5 style={{color: "#333333"}}>Our Customers Always</h5>
-            <h5 style={{color: "#333333"}}>100% Satisfied</h5>
+            <h5>Our Customers Always</h5>
+            <h5>100% Satisfied</h5>
             <div className='p-2'><img style={{height: "15px"}} src='https://snipboard.io/NRTeph.jpg'/></div>
-            <p style={{color: "#666666"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit sed don eiusmod tempor enim minim veniam quis notrud exercitation</p>
+            <p className='par'>Lorem ipsum dolor sit amet consectetur adipisicing elit sed don eiusmod tempor enim minim veniam quis notrud exercitation</p>
         </Col>
         <Col sm lg="3">
             <img src='https://autostar.templines.org/wp-content/uploads/2018/12/as002.jpg' />
-            <h5 style={{color: "#333333"}}>We Provide Easier </h5>
-            <h5 style={{color: "#333333"}}>&#38; Faster Bookings</h5>
+            <h5>We Provide Easier </h5>
+            <h5>&#38; Faster Bookings</h5>
             <div className='p-2'><img style={{height: "15px"}} src='https://snipboard.io/NRTeph.jpg'/></div>
-            <p style={{color: "#666666"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit sed don eiusmod tempor enim minim veniam quis notrud exercitation</p>
+            <p className='par'>Lorem ipsum dolor sit amet consectetur adipisicing elit sed don eiusmod tempor enim minim veniam quis notrud exercitation</p>
         </Col>
         <Col sm lg="3">
             <img src='https://autostar.templines.org/wp-content/uploads/2018/12/as003.jpg' />
-            <h5 style={{color: "#333333"}}>Your Choice of</h5>
-            <h5 style={{color: "#333333"}}>Any Pickup Location</h5>
+            <h5>Your Choice of</h5>
+            <h5>Any Pickup Location</h5>
             <div className='p-2'><img style={{height: "15px"}} src='https://snipboard.io/NRTeph.jpg'/></div>
-            <p style={{color: "#666666"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit sed don eiusmod tempor enim minim veniam quis notrud exercitation</p>
+            <p className='par'>Lorem ipsum dolor sit amet consectetur adipisicing elit sed don eiusmod tempor enim minim veniam quis notrud exercitation</p>
         </Col>
       </Row>
     </Container>
---
+++ src/components/Rentalworks.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import '../css/rentalworks.css';
+import divBg from '../media/rentalworks.mp4';
+
+const Rentalworks = () => {
+  return (
+    <div className='main'>
+      <div className='overlay'></div>
+      <div>
+      <div className='banner'><video src={divBg} autoPlay loop muted /></div>
+        <div className='content'>
+          <div className='title'><h2>HOW DOES THE<br/>RENTCAR RENTAL WORKS</h2></div>
+          <div className='contenta'>
+              <div className='contentimg'>
+                <div><img src='https://autostar.templines.org/wp-content/uploads/2018/12/steps01.png'/></div>
+              </div>
+              <div className='contentp'>
+                <div><p>Once selected, Book<br/>our auto and set a<br/>pickup date / time</p></div>
+              </div>
+          </div>
+          <div className='contenta'>
+              <div className='contentimg'>
+                <div><img src='https://autostar.templines.org/wp-content/uploads/2018/12/steps02.png'/></div>
+              </div>
+              <div className='contentp'>
+                <div><p>Once selected, Book<br/>our auto and set a<br/>pickup date / time</p></div>
+              </div>
+          </div>
+          <div className='contenta'>
+              <div className='contentimg'>
+                <div><img src='https://autostar.templines.org/wp-content/uploads/2018/12/steps03.png'/></div>
+              </div>
+              <div className='contentp'>
+                <div><p>Once selected, Book<br/>our auto and set a<br/>pickup date / time</p></div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Rentalworks
